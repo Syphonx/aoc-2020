02154be6c48b8bc37ee56ef3ab430f77c6e6a9bf
Jak Barnes <contact@jakbarnes.co.uk>
2020-12-23 16:38:56 +0000

    day 16 part 2

--- src/day16.cpp
+++ src/day16.cpp
@@ -43,15 +43,18 @@ struct Rule
 {
 	string			Key;
 	FieldRange		Range;
+	int32			Index;
 
 	Rule()
 	{
+		Index = -1;
 	}
 
 	Rule(string NewKey, FieldRange& NewRange)
 	{
 		Key = NewKey;
 		Range = std::move(NewRange);
+		Index = -1;
 	}
 
 	bool IsMatch(int32 number) const
@@ -79,6 +82,32 @@ struct Ticket
 				Numbers.push_back(stoi(r));
 			}
 		}
+	}
+
+	bool HasScanningError(const vector<Rule>& rules) const
+	{
+		bool bHasError = false;
+
+		for (const int32 number : Numbers)
+		{
+			bool bMatch = false;
+			for (const auto& rule : rules)
+			{
+				if (rule.IsMatch(number))
+				{
+					bMatch = true;
+					break;
+				}
+			}
+
+			if (!bMatch)
+			{
+				bHasError = true;
+				break;
+			}
+		}
+
+		return bHasError;
 	}
 
 	int32 GetScanningError(const vector<Rule>& rules) const
@@ -113,6 +142,30 @@ enum class ParseStage : uint8
 	Ticket,
 	Nearby
 };
+
+static bool		AllRulesFound(vector<Rule>& rules, vector<int32>& filter)
+{
+	bool bFound = true;
+	filter.clear();
+
+	for (int32 i = 0; i < rules.size(); i++)
+	{
+		auto& rule = rules[i];
+
+		if (rule.Index == -1)
+		{
+			bFound = false;
+		}
+		else
+		{
+			filter.push_back(rule.Index);
+		}
+	}
+
+	std::sort(filter.begin(), filter.end());
+
+	return bFound;
+}
 
 static int32	Solve_1(vector<string> input)
 {
@@ -194,11 +247,158 @@ static int32	Solve_1(vector<string> input)
 	return answer;
 }
 
-static int32	Solve_2(vector<string> input)
+static uint64	Solve_2(vector<string> input)
 {
-	int32	answer = 0;
+	uint64				answer = 0;
 
-	printf("Answer: %d\n", answer);
+	vector<string>		parts;
+	vector<string>		rangeParts;
+	vector<Rule>		rules;
+	vector<Ticket>		tickets;
+	Ticket				myTicket;
+	ParseStage			stage = ParseStage::Rules;
+
+	for (const auto& line : input)
+	{
+		if (line == "")
+		{
+			stage = static_cast<ParseStage>(((int32)stage) + 1);
+			continue;
+		}
+
+		switch (stage)
+		{
+			default:
+			case ParseStage::Rules:
+			{
+				parts.clear();
+				rangeParts.clear();
+
+				if (Split(parts, line, ":"))
+				{
+					Trim(parts[0]);
+					Remove(parts[0], " ");
+
+					FieldRange ranges;
+					if (Split(rangeParts, parts[1], "or"))
+					{
+						for (auto& r : rangeParts)
+						{
+							Trim(r);
+							Remove(r, " ");
+						}
+						ranges.Parse(rangeParts[0], rangeParts[1]);
+					}
+
+					rules.push_back({ parts[0], ranges });
+				}
+			}
+			break;
+
+			case ParseStage::Ticket:
+			{
+				if (line.find("your") == -1)
+				{
+					myTicket = Ticket(line);
+					//tickets.emplace_back(Ticket(line));
+				}
+			}
+			break;
+
+			case ParseStage::Nearby:
+			{
+				if (line.find("nearby") == -1)
+				{
+					tickets.emplace_back(Ticket(line));
+				}
+			}
+			break;
+		}
+	}
+
+	// Remove invalid tickets
+	for (int32 i = tickets.size() - 1; i >= 0; i--)
+	{
+		if (tickets[i].HasScanningError(rules))
+		{
+			tickets.erase(tickets.begin() + i);
+		}
+	}
+
+	// Match rules to indices
+	vector<int32> filter;
+	vector<int32> counts;
+	counts.resize(tickets[0].Numbers.size());
+
+	while (!AllRulesFound(rules, filter))
+	{
+		// 0-5 or 8-19
+		for (int32 r = 0; r < rules.size(); r++)
+		{
+			Rule& rule = rules[r];
+
+			if (rule.Index != -1)
+			{
+				continue;
+			}
+
+			std::fill(counts.begin(), counts.end(), 0);
+
+			for (size_t n = 0; n < counts.size(); n++)
+			{
+				auto iter = std::find(filter.begin(), filter.end(), n);
+				if (iter != filter.end())
+				{
+					continue;
+				}
+
+				bool bValid = true;
+				for (auto& ticket : tickets)
+				{
+					if (!rule.IsMatch(ticket.Numbers[n]))
+					{
+						bValid = false;
+						break;
+					}
+				}
+
+				if (bValid)
+				{
+					counts[n]++;
+				}
+			}
+
+			int32 sum = 0;
+			for (size_t n = 0; n < counts.size(); n++)
+			{
+				sum += counts[n];
+			}
+			
+			if (sum == 1)
+			{
+				for (size_t n = 0; n < counts.size(); n++)
+				{
+					if (counts[n] == 1)
+					{
+						rule.Index = n;
+						filter.push_back(n);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	answer = 1;
+	for (const auto& rule : rules)
+	{
+		if (rule.Key.find("departure") != -1)
+		{
+			answer *= myTicket.Numbers[rule.Index];
+		}
+	}
+
+	printf("Answer: %llu\n", answer);
 
 	return answer;
 }
@@ -228,7 +428,18 @@ void Day16::Part2()
 {
 	assert(Solve_2(
 		{
+			"class: 0-1 or 4-19",
+			"row: 0-5 or 8-19",
+			"seat: 0-13 or 16-19",
+			"",
+			"your ticket:",
+			"11,12,13",
+			"",
+			"nearby tickets:",
+			"3,9,18",
+			"15,1,5",
+			"5,14,9"
 		}
-	) == 0);
+	) != 0);
 	Solve_2(GetStrInput("inputs/Day16.txt"));
 }
